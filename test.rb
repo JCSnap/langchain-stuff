7ff831eb32321528d78335828f7ffa33f25313c3
JCSnap <e0970167@u.nus.edu>
2023-07-13 10:59:28 +0800

    tested pdf

--- test.rb
+++ test.rb
@@ -1,12 +1,19 @@
 require "langchain"
 
+wikipedia = Langchain::Tool::Wikipedia.new
 calculator = Langchain::Tool::Calculator.new
 chat = Langchain::LLM::OpenAI.new(api_key: ENV["OPENAI_API_KEY"])
 
 agent = Langchain::Agent::ReActAgent.new(
     llm: chat,
-    tools: [calculator]
+    tools: [wikipedia, calculator]
   )
 
-output = agent.run(question: "What is 5^0.567?")
-puts output
+# output = agent.run(question: "WHo won the 2022 world cup?")
+# puts output
+pdf = Langchain::Loader.load('https://cabmakassar.org/wp-content/uploads/2020/05/The-Philosophy-Book-DK.pdf')
+all_text = ""
+pdf.pages.each do |page|
+    all_text << page.text
+end
+puts all_text
